f1f1e5a55e88e9736c30378a0b76e6def86445bd
Zak Clark <zak@zakariahclark.com>
2019-05-09 12:52:16 -0700

    database schema updates

--- data/burger_db.sql
+++ data/burger_db.sql
@@ -1,11 +1,21 @@
-create database if not exists burgers_db;
-use burgers_db;
+-- create database if not exists burgers_db;
+-- use burgers_db;
+
+-- create table burgers (
+-- 	id int not null auto_increment,
+--     burger_name varchar(100) not null,
+--     devoured bool default false,
+--     primary key (id)
+-- );
 
 create table burgers (
-	id int not null auto_increment,
+	id int auto_increment not null ,
     burger_name varchar(100) not null,
     devoured bool default false,
+    createdAt timestamp not null,
     primary key (id)
 );
+
+
 
 
