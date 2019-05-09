4e0709319c4858773bbef56c944b7eefe7b5f9f4
Zak Clark <zak@zakariahclark.com>
2019-05-09 11:42:19 -0700

    JAWS updates

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,28 @@
 var mysql = require("mysql");
 
 //Connection info
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root1234!",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "root1234!",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL){
+
+} else {
+
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root1234!",
+    database: "burgers_db"
+  });
+
+};
+
 
 //Connect confirmation
 connection.connect(function(err) {
