3b55f01eb42a5d476cac8bcb812f9846c1ec7646
Zak Clark <zak@zakariahclark.com>
2019-05-09 11:23:19 -0700

    database info

---
+++ data/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name, devoured) values ("Veggie Burger", true);
+insert into burgers (burger_name, devoured) values ("Trash Burger", false);
+insert into burgers (burger_name, devoured) values ("Mystery Burger", false);
